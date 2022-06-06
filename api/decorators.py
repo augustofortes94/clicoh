2aab6596cd8fbf7054ec19f368f399117e4422fb
Fortes <fortesa@spa1.gba.gov.ar>
2022-06-06 08:42:53 -0300

    refactoring of login decorator

--- api/decorators.py
+++ api/decorators.py
@@ -1,19 +1,18 @@
 from functools import wraps
-from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.response import Response
 import jwt
 
 def api_login_required(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
         token = request.request.COOKIES.get('jwt')
-        if token == None:
-            return JsonResponse({'message':"Error: Unauthenticated..."})
-        try:
-            payload = jwt.decode(token, 'secret', algorithms=['HS256'])     #if the token can be decoded, it is a valid token
-        except Exception: #jwt.ExpiredSignatureError:
-            return JsonResponse({'message':"Error: Unauthenticated..."})    #if the token is expired or something else, refuse
-        if payload:
-            return function(request, *args, **kwargs)
-        else:
-            return JsonResponse({'message':"Error: Unauthenticated..."})
+        if token != None:
+            try:
+                payload = jwt.decode(token, 'secret', algorithms=['HS256'])     #if the token can be decoded, it is a valid token
+            except Exception: #jwt.ExpiredSignatureError:
+                return Response({'message':"Error: Unauthenticated..."}, status=status.HTTP_401_UNAUTHORIZED)    #if the token is expired or something else, refuse
+            if payload:
+                return function(request, *args, **kwargs)
+        return Response({'message':"Error: Unauthenticated..."}, status=status.HTTP_401_UNAUTHORIZED)
     return wrap
